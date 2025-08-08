082e17b014a48dfc7ab37b9139f6a52714c5e5dc
bppleman <bppleman@gmail.com>
2025-08-08 08:52:01 +0800

    <refactor> 重构 BorderStyle 和 CommentStyle 的模块结构，优化代码组织 <feat> 添加 CommentStyle 结构及相关功能 <feat> 更新 Container 和 Text 组件以支持新的样式接口

--- rush-say/src/bubble.rs
+++ rush-say/src/bubble.rs
@@ -1,4 +1,4 @@
-use crate::border::CommentStyle;
+use crate::style::{BorderStyle, CommentStyle};
 use crate::widget::{Align, Constraints, Container, Size, Text, Widget};
 use std::io::Write;
 
@@ -6,27 +6,56 @@ use std::io::Write;
 pub struct Bubble {
     pub inner: Container<Text>,
     pub comment: Option<CommentStyle>,
+    pub width: usize,
 }
 
 impl Bubble {
-    // pub fn new(width: usize, padding: usize, border_style: BorderStyle, comment_style: Option<CommentStyle>) -> Self {
-    //     Bubble {
-    //         style: BubbleStyle { width, padding, border_style },
-    //         comment_style,
-    //     }
-    // }
-
-    pub fn say(&self, writer: &mut impl std::io::Write, content: impl AsRef<str>) -> std::io::Result<()> {
-        todo!()
+    pub fn new(
+        text: impl AsRef<str>,
+        width: usize,
+        padding: usize,
+        margin: usize,
+        border: BorderStyle,
+        comment: Option<CommentStyle>,
+    ) -> Self {
+        Bubble {
+            inner: Container::new(Text::new(text))
+                .set_border(border)
+                .set_padding(padding)
+                .set_margin(margin)
+                .set_align(Align::Center),
+            comment,
+            width,
+        }
     }
-}
 
-impl Widget for Bubble {
-    fn layout(&mut self, constraints: Constraints) -> Size {
-        todo!()
-    }
-
-    fn render(&self, writer: &mut impl Write, row: usize) -> std::io::Result<()> {
-        todo!()
+    pub fn say(mut self, writer: &mut impl Write) -> std::io::Result<()> {
+        let constraints = Constraints {
+            render_width: self.width,
+            wrap_width: self.width,
+        };
+        self.inner.layout(constraints);
+        let size = match &self.comment {
+            Some(comment) => Size {
+                width: self.inner.size().width + comment.size(),
+                height: self.inner.size().height,
+            },
+            None => self.inner.size(),
+        };
+        for row in 0..size.height {
+            match &self.comment {
+                None => self.inner.render(writer, row)?,
+                Some(comment) => {
+                    write!(writer, "{}", comment.prefix)?;
+                    self.inner.render(writer, row)?;
+                    if let Some(suffix) = comment.suffix {
+                        writeln!(writer, "{}", suffix)?;
+                    } else {
+                        writeln!(writer)?;
+                    }
+                }
+            }
+        }
+        Ok(())
     }
 }
